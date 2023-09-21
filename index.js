725b4cd378e2c22e1a59b70f11cdab84521ce18f
Damien Brockmann <damien@bigblueshoes.co>
2023-09-21 14:49:03 -0500

    exports fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const consistentHash = require("./consistentHash");
-const getBrokers = require("./getBrokers");
-const tcpClient = require("./tcpClient");
-const tcpServer = require("./tcpServer");
+const { consistentHash } = require("./consistentHash");
+const { getBrokers } = require("./getBrokers");
+const { tcpClient } = require("./tcpClient");
+const { tcpServer } = require("./tcpServer");
 
 module.exports = {
     consistentHash,
