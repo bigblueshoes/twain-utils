36ca3f69ec3a7209e373956b4a1e6c8364ef857d
Damien Brockmann <damien@bigblueshoes.co>
2024-01-29 14:41:02 -0600

    sendAndEnd test

--- tcpClient.js
+++ tcpClient.js
@@ -23,12 +23,23 @@ const tcpClient = (host, port, opts= { keepConnection: true }) => {
         client.write(buffer);
       });
     }
+    const sendAndEnd = (data) => {
+      return new Promise(resolve => {
+        const buffer = Buffer.from(JSON.stringify(data));
+        client.on("data", (returnedData) => {
+          const response = JSON.parse(returnedData.toString("utf-8"));
+          resolve(response);
+        });
+        client.write(buffer);
+        end();
+      });
+    }
     const init = async (init_data) => {
       connect();
       if (opts.keepConnection) client.on('close', () => init(init_data));
       return await send(init_data);
     }
-    return { init, send, connect, end, client }
+    return { init, send, connect, end, sendAndEnd, client }
 }
 
 exports.tcpClient = tcpClient;
