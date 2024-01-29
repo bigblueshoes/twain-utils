6a547edb96f798e7a0c7874ebbb1c950de64dd27
Damien Brockmann <damien@bigblueshoes.co>
2024-01-29 14:28:54 -0600

    revert

--- tcpClient.js
+++ tcpClient.js
@@ -16,17 +16,11 @@ const tcpClient = (host, port, opts= { keepConnection: true }) => {
     const send = (data) => {
       return new Promise(resolve => {
         const buffer = Buffer.from(JSON.stringify(data));
-        console.log("buffer length", buffer.length);
-        console.log("isValidJSON", isValidJSON(buffer));
-        console.log("buffer", buffer);
         client.on("data", (returnedData) => {
-          console.log("returned data", returnedData);
           const response = JSON.parse(returnedData.toString("utf-8"));
           resolve(response);
         });
-        client.write(buffer, 'utf8', () => {
-          client.end();
-        });
+        client.write(buffer);
       });
     }
     const init = async (init_data) => {
